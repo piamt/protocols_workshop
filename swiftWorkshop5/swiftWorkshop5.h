656613ee2e5b35b993077d439411eef6177b61ee
Pia Muñoz <pmunoz@naradarobotics.com>
2016-11-28 12:00:44 +0100

    empty project cocoa framework

---
+++ swiftWorkshop5/swiftWorkshop5.h
@@ -0,0 +1,19 @@
+//
+//  swiftWorkshop5.h
+//  swiftWorkshop5
+//
+//  Created by Pia Muñoz on 28/11/16.
+//  Copyright © 2016 iOSWorkshops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftWorkshop5.
+FOUNDATION_EXPORT double swiftWorkshop5VersionNumber;
+
+//! Project version string for swiftWorkshop5.
+FOUNDATION_EXPORT const unsigned char swiftWorkshop5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftWorkshop5/PublicHeader.h>
+
+
